f93aaad227c4744d1c53ead0094b75f23079f69a
s3878694 <s3878694@rmit.edu.vn>
2022-07-22 10:55:48 +0700

    Initial Commit

---
+++ Asm1IOS/Asm1IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asm1IOSApp.swift
+//  Asm1IOS
+//
+//  Created by Long Nguyen on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Asm1IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
